3aaa4cc20f6bceba4080a099a6ae21a319a1a538
Avalondy <duyao2016@gmail.com>
2018-02-17 16:09:37 -0500

    class list template

--- listTemp.cpp
+++ listTemp.cpp
@@ -13,6 +13,9 @@ template <class T>
 class list{
     public:
         list():head(nullptr), cursor(nullptr){}
+        list(const list&);
+//        list(const T& a[], int);
+        ~list();
         void prepend(T n); //insert at front value n
         T get_element(){return cursor->d;}
         void advance(){cursor = cursor->next;}
@@ -23,8 +26,53 @@ class list{
 };
 
 template <class T>
+list<T>::list(const list& lst){
+    if(lst.head == nullptr){head = nullptr; cursor = nullptr;}
+    else{
+        cursor = lst.head;
+        list_element<T>* h = new list_element<T>();
+        list_element<T>* previous;
+        head = h;
+        h->d = lst.head->d;
+        previous = h;
+        for(cursor = cursor->next; cursor != nullptr; ){
+            h = new list_element<T>();
+            h->d = cursor->d;
+            previous->next = h;
+            cursor = cursor->next;
+            previous = h;
+        }
+    cursor = head;
+    }
+}
+
+/*template <class T>
+list<T>::list(const T& a[], int n){
+    list_element<T>* h = new list_element<T>();
+    h->d = a[0];
+    head = h;
+    cursor = head;
+    for(int i=1; i<n; ++i){
+        h = new list_element<T>(n=a[i]);
+        cursor->next = h;
+       cursor = h;
+    }
+    cursor = head;
+}*/
+
+template <class T>
+list<T>::~list(){
+    for(cursor = head; cursor != 0; ){
+        cursor = head->next;
+        delete head;
+        head = cursor;
+    }
+    cout << "Destructed!" << endl;
+}
+
+template <class T>
 void list<T>::prepend(T n){
-    if(head == 0)
+    if(head == nullptr)
         cursor = head = new list_element<T>(n, head);
     else
         head = new list_element<T>(n, head);
@@ -33,7 +81,7 @@ void list<T>::prepend(T n){
 template <class T>
 void list<T>::print(){
     list_element<T>* h = head;
-    while(h != 0){
+    while(h != nullptr){
         cout << h->d << ", ";
         h = h->next;
     }
@@ -42,19 +90,10 @@ void list<T>::print(){
 
 int main(){
     list<int> a, b;
-    list<string> c;
+    int data[10] = {3,4,6,7,-3,5};
+//    list<int> d(data, 6);
+//    list<int> e(data, 10);
     a.prepend(9); a.prepend(8);
     cout << "list a" << endl;
     a.print();
-    for(int i=0; i<40; ++i)
-        b.prepend(i*i);
-    cout << "list b" << endl;
-    b.print();
-    c.prepend("abcd");
-    cout << "list c" << endl;
-    c.print();
-    list<string> d = c;
-    d.prepend("e");
-    cout << "list d" << endl;
-    d.print();
 }
