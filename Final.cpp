e18c760074875c83494367f76785a1db9f80b01b
Avalondy <duyao2016@gmail.com>
2018-03-05 16:21:57 -0500

    final

---
+++ Final.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=2, b=-3, c=2;
+    bool tval1=false, tval2=true;
+    char ch='b';
+    cout << b+c*a << endl;
+    cout << c % 5/2 << endl;
+    cout << -a*c++ << endl;
+    cout << tval1 && tval2 << endl;
+}
