bb44b77ef70f85014522676e190a900b4dfd525a
Avalondy <duyao2016@gmail.com>
2018-03-07 17:33:20 -0500

    old style for_each

---
+++ oldFor_each.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void incr(int &i){static int n=1; i=n++;}
+void outvec(int i){cout << i << endl;}
+
+int main(){
+    vector<int> v(6);
+    for_each(v.begin(), v.end(), incr);
+    for_each(v.begin(), v.end(), outvec);
+}
