760494306b0e10bb4daa1d2be23520e9f543b7c4
Avalondy <duyao2016@gmail.com>
2018-03-19 13:52:13 -0400

    inherited to 3d points

--- duoInhe.cpp
+++ duoInhe.cpp
@@ -19,12 +19,26 @@ class point:public duo<T>{
     public:
         using duo<T>::first;
         using duo<T>::second;
-        T length(){return sqrt(first*first + second* second);}
+        virtual T length(){return sqrt(first*first + second* second);}
 };
 
+template <class T>
+class point3d:public point<T>{
+    public:
+        point3d():point<T>::point(), z(0.0){}
+        using point<T>::first;
+        using point<T>::second;
+        T length(){return sqrt(first*first + second*second + z*z);}
+        void set_z(T d){z = d;}
+    private:
+        T z;
+};
+
+
+
 int main(){
-    point<double> q;
-    q.set_first(3.0); q.set_second(4.0);
+    point3d<double> q;
+    q.set_first(1.0); q.set_second(1.0); q.set_z(1.0);
     cout << q.get_first() << ", " << q.get_second() << endl;
     cout << q.length() << endl;
     return 0;
